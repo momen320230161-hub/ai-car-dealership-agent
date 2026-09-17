45143e567b9e85db3768cc05db75acdeefbc7e85
Mo'men Ismail <momen.320230161@ejust.edu.eg>
2026-09-17 22:39:12 +0300

    feat(security): install app-wide browser headers [skip ci]

--- app/blueprints/site/__init__.py
+++ app/blueprints/site/__init__.py
@@ -2,7 +2,10 @@
 
 from flask import Blueprint
 
+from app.common.security import register_security_hooks
+
 bp = Blueprint("site", __name__)
+register_security_hooks(bp)
 
 from app.blueprints.admin import bp as admin_bp  # noqa: E402
 from app.blueprints.site import routes  # noqa: E402, F401
