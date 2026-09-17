a704a05e9dc9f14ad067b4f98ec19d30fd3654e3
Mo'men Ismail <momen.320230161@ejust.edu.eg>
2026-09-17 22:06:38 +0300

    style(admin): sort nested blueprint imports

--- app/blueprints/site/__init__.py
+++ app/blueprints/site/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 
 bp = Blueprint("site", __name__)
 
-from app.blueprints.site import routes  # noqa: E402, F401
 from app.blueprints.admin import bp as admin_bp  # noqa: E402
+from app.blueprints.site import routes  # noqa: E402, F401
 
 bp.register_blueprint(admin_bp)
